d3697361f804d0582c4df8d3ba2b58faad9ee97d
sahomelabs <sahomelabs@gmail.com>
2023-12-28 18:48:35 -0500

    update show

--- views/Show.jsx
+++ views/Show.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 function Show(props) {
   return (
     <>
-    The 
-        <h1>{props.fruit.name}</h1>is 
+        <h1>{props.fruit.name}</h1>Color
         <h1 style={{color:`${props.fruit.color}`}}>{props.fruit.color}</h1>
          <h1>{props.fruit.readyToEat? "Add to Cart" : " It needs a bit more time Before its Edible"}</h1>
     
