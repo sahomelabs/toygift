c53e4ec69cd457b4238bf93b4025fbb7b30a1420
sahomelabs <sahomelabs@gmail.com>
2023-12-28 17:49:56 -0500

    added more toys

--- models/fruits.js
+++ models/fruits.js
@@ -29,7 +29,14 @@ const fruits = [
         name:'Street Fighter Turbo Champion Legacy',
         color: 'orange',
         description: 'Logitech G29 Driving Force Steering Wheel for PS5 | PS4 | PS3 | PC'
+    },
+
+    {
+        name:' The Orient Express Train',
+        color: 'orange',
+        description: 'All aboard There are also 8 LEGO minifigures'
     }
+
     
 ];
 
