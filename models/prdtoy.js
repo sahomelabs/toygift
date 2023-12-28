05d03f337ec32ac8568f6e5a39bc0379cb24786f
sahomelabs <sahomelabs@gmail.com>
2023-12-28 18:53:58 -0500

    update prd descriptin

--- models/prdtoy.js
+++ models/prdtoy.js
@@ -5,36 +5,38 @@ const prdtoy = [
     {
         name:'Tobbi Licensed Mercedes Benz AMG Electric Kids Ride On Car, Battery Powered Ride On Toy for Toddlers with Parental Remote, White',
         color: 'red',
-        readyToEat: true
+        addToGiftBucketList: true
     },
     {
         name:'Sony PlayStation 5 Slim Disc Console with Spiderman 2',
-        color: 'green',
-        readyToEat: false
+        color: 'white',
+        addToGiftBucketList: false
     },
 
     {
         name:'Americas Toys Project Ride On Toy Motorcycle With Upgraded Dashboard',
         color: 'Black',
-        readyToEat: true
+        addToGiftBucketList: true
     },
 
     {
         name:'Nintendo Switch Joy-Con',
         color: 'orange',
-        readyToEat: false
+        addToGiftBucketList: false
     },
 
     {
         name:'Street Fighter Turbo Champion Legacy',
         color: 'orange',
-        description: 'Logitech G29 Driving Force Steering Wheel for PS5 | PS4 | PS3 | PC'
+        description: 'Logitech G29 Driving Force Steering Wheel for PS5 | PS4 | PS3 | PC',
+        addToGiftBucketList: false
     },
 
     {
         name:' The Orient Express Train',
         color: 'orange',
-        description: 'All aboard There are also 8 LEGO minifigures'
+        description: 'All aboard There are also 8 LEGO minifigures',
+        addToGiftBucketList: false
     }
 
     
