0ba5368bad20fbdc61b71d736d4618e94c2e3594
sahomelabs <sahomelabs@gmail.com>
2023-12-28 20:02:06 -0500

    update showjsx with new details

--- views/Show.jsx
+++ views/Show.jsx
@@ -5,7 +5,7 @@ function Show(props) {
     <>
         <h1>{props.fruit.name}</h1>Color
         <h1 style={{color:`${props.fruit.color}`}}>{props.fruit.color}</h1>
-         <h1>{props.fruit.readyToEat? "Add to Cart" : " It needs a bit more time Before its Edible"}</h1>
+         <h1>{props.fruit.readyToEat? "Add to Cart" : " You will get this product during next christmas holiday"}</h1>
     
     </>
   )
