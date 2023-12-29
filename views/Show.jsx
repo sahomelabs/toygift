1754bb161aaa054a4a2e2b309526acf20d819529
sahomelabs <sahomelabs@gmail.com>
2023-12-28 20:07:39 -0500

    update showjsx

--- views/Show.jsx
+++ views/Show.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function Show(props) {
   return (
     <>
-        <h1>{props.fruit.name}</h1>Color
-        <h1 style={{color:`${props.fruit.color}`}}>{props.fruit.color}</h1>
-         <h1>{props.fruit.readyToEat? "Add to Cart" : " You will get this product during next christmas holiday"}</h1>
+        <h1>{props.toy.name}</h1>Color
+        <h1 style={{color:`${props.toy.color}`}}>{props.toy.color}</h1>
+         <h1>{props.toy.readyToEat? "Add to Cart" : " You will get this product during next christmas holiday"}</h1>
     
     </>
   )
